718dc4e792a2692750c5548313e1ca68cfecfe8f
Ricardo Zaideman <ricardo.charf@movile.com>
2019-02-14 13:42:20 -0200

    Startted fixing zoom to fit content when zoom and pan

--- Assets/VisualNodeSystem/Example/ScaleTest.cs
+++ Assets/VisualNodeSystem/Example/ScaleTest.cs
@@ -14,16 +14,16 @@ public class ZoomTestWindow : EditorWindow
         window.Show();
     }
 
-    private EditorZoomArea editorZoomArea;
+    private EditorZoomArea editorZoomArea = new EditorZoomArea();
 
     private void DrawZoomArea()
     {
         // Within the zoom area all coordinates are relative to the top left corner of the zoom area
         // with the width and height being scaled versions of the original/unzoomed area's width and height.
-        editorZoomArea.Begin();
+        editorZoomArea.Begin(new Rect(Vector2.zero, position.size), new Rect(Vector2.zero, position.size));
 
         // You can also use GUILayout inside the zoomed area.
-        GUI.Box(new Rect(100.0f, 100.0f, 100.0f, 25.0f), "Zoomed Box");
+        GUI.Box(new Rect(Vector2.zero, position.size), "Zoomed Box");
         GUILayout.Button("Zoomed Button 1");
         GUILayout.Button("Zoomed Button 2");
 
@@ -38,11 +38,6 @@ public class ZoomTestWindow : EditorWindow
 
     public void OnGUI()
     {
-        if (editorZoomArea == null)
-        {
-            editorZoomArea = new EditorZoomArea(new Rect(0.0f, 0f, position.width, position.height));
-        }
-
         // The zoom area clipping is sometimes not fully confined to the passed in rectangle. At certain
         // zoom levels you will get a line of pixels rendered outside of the passed in area because of
         // floating point imprecision in the scaling. Therefore, it is recommended to draw the zoom
@@ -57,22 +52,21 @@ public class EditorZoomArea
     private Matrix4x4 _prevGuiMatrix;
     private Vector2 _zoomCoordsOrigin = Vector2.zero;
     private float _zoom = 1f;
-    private Rect _zoomArea = new Rect(0,0,200,200);
+    private Rect _viewArea = Rect.zero;
+    private Rect _contentArea = Rect.zero;
     private const float kZoomMin = 0.1f;
     private const float kZoomMax = 10.0f;
     private const float wheelSensibility = 100.0f;
 
-    public EditorZoomArea(Rect position)
+    public Rect Begin(Rect viewArea, Rect contentArea)
     {
-        _zoomArea = position;
-    }
-
-    public Rect Begin()
-    {
+        _viewArea = viewArea;
+        _contentArea = contentArea;
+        _contentArea.position = Vector2.zero;
         HandleEvents();
         GUI.EndGroup();        // End the group Unity begins automatically for an EditorWindow to clip out the window tab. This allows us to draw outside of the size of the EditorWindow.
 
-        Rect clippedArea = _zoomArea.ScaleSizeBy(1.0f / _zoom, _zoomArea.TopLeft());
+        Rect clippedArea = _viewArea.ScaleSizeBy(1.0f / _zoom, _viewArea.TopLeft());
         clippedArea.y += kEditorWindowTabHeight;
         GUI.BeginGroup(clippedArea);
 
@@ -81,7 +75,7 @@ public class EditorZoomArea
         Matrix4x4 scale = Matrix4x4.Scale(new Vector3(_zoom, _zoom, 1.0f));
         GUI.matrix = translation * scale * translation.inverse * GUI.matrix;
 
-        GUILayout.BeginArea(new Rect(_zoomCoordsOrigin.x, _zoomCoordsOrigin.y, _zoomArea.width, _zoomArea.height));
+        GUILayout.BeginArea(new Rect(_zoomCoordsOrigin.x, _zoomCoordsOrigin.y, _viewArea.width, _viewArea.height));
 
         return clippedArea;
     }
@@ -98,11 +92,16 @@ public class EditorZoomArea
             Vector2 delta = Event.current.delta;
             Vector2 zoomCoordsMousePos = ConvertScreenCoordsToZoomCoords(screenCoordsMousePos);
             float zoomDelta = -delta.y / (wheelSensibility * 1f/_zoom);
+            if(_zoom + zoomDelta < 1){
+                zoomDelta = 0;
+                _zoom = 1;
+            }
             float oldZoom = _zoom;
             _zoom += zoomDelta;
             _zoom = Mathf.Clamp(_zoom, kZoomMin, kZoomMax);
-            var lala = (_zoom / oldZoom) * (zoomCoordsMousePos) - zoomCoordsMousePos;
-            _zoomCoordsOrigin -= lala;
+            Vector2 reference = zoomCoordsMousePos;
+            var movementVector = (_zoom / oldZoom) * (reference) - reference;
+            _zoomCoordsOrigin -= movementVector;
             _zoomCoordsOrigin.x = Mathf.Min(0f, _zoomCoordsOrigin.x);
             _zoomCoordsOrigin.y = Mathf.Min(0f, _zoomCoordsOrigin.y);
             Event.current.Use();
@@ -116,16 +115,26 @@ public class EditorZoomArea
         {
             Vector2 delta = Event.current.delta;
             delta /= _zoom;
-            _zoomCoordsOrigin += delta;
-            _zoomCoordsOrigin.x = Mathf.Min(0f, _zoomCoordsOrigin.x);
-            _zoomCoordsOrigin.y = Mathf.Min(0f, _zoomCoordsOrigin.y);
+            Rect zoomMovedContentArea = new Rect(_zoomCoordsOrigin + delta, _contentArea.ScaleSizeBy(_zoom, Vector2.zero).size);
+            zoomMovedContentArea.x = Mathf.Min(0f, zoomMovedContentArea.x);
+            zoomMovedContentArea.y = Mathf.Min(0f, zoomMovedContentArea.y);
+            //var mov = zoomMovedContentArea.BotRight() - _viewArea.BotRight();
+            //if (mov.x >= 0)
+            //{
+            //    _zoomCoordsOrigin.x = zoomMovedContentArea.position.x;
+            //}
+            //if (mov.y >= 0)
+            //{
+            //    _zoomCoordsOrigin.y = zoomMovedContentArea.position.y;
+            //}
+            _zoomCoordsOrigin = zoomMovedContentArea.position;
             Event.current.Use();
         }
     }
 
     private Vector2 ConvertScreenCoordsToZoomCoords(Vector2 screenCoords)
     {
-        return (screenCoords - _zoomArea.TopLeft()) / _zoom;
+        return (screenCoords - _viewArea.TopLeft()) / _zoom;
     }
 
     public void ResetZoom()
@@ -148,6 +157,10 @@ public static class RectExtensions
     public static Vector2 TopLeft(this Rect rect)
     {
         return new Vector2(rect.xMin, rect.yMin);
+    }
+    public static Vector2 BotRight(this Rect rect)
+    {
+        return new Vector2(rect.xMax, rect.yMax);
     }
     public static Rect ScaleSizeBy(this Rect rect, float scale)
     {
@@ -182,6 +195,10 @@ public static class RectExtensions
         result.x += pivotPoint.x;
         result.y += pivotPoint.y;
         return result;
+    }
+    public static bool IsInside(this Rect rect, Rect other)
+    {
+        return other.Contains(rect.TopLeft()) && other.Contains(rect.BotRight());
     }
 }
  
