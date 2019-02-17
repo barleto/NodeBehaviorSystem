1e7570940c89b1536eba4618cd7bd4024a302138
Ricardo Charf <ricardo.charf@wavy.global>
2019-02-17 12:20:54 -0300

    beign adding support for scrollbar

--- Assets/VisualNodeSystem/Editor/ScaleTest.cs
+++ Assets/VisualNodeSystem/Editor/ScaleTest.cs
@@ -20,10 +20,10 @@ public class ZoomTestWindow : EditorWindow
     {
         // Within the zoom area all coordinates are relative to the top left corner of the zoom area
         // with the width and height being scaled versions of the original/unzoomed area's width and height.
-        editorZoomArea.Begin(new Rect(Vector2.zero, position.size), new Rect(Vector2.zero, position.size));
+        editorZoomArea.Begin(new Rect(Vector2.zero, position.size), new Rect(Vector2.zero, position.size*2));
 
         // You can also use GUILayout inside the zoomed area.
-        GUI.Box(new Rect(Vector2.zero, position.size), "Zoomed Box");
+        GUI.Box(new Rect(Vector2.zero, position.size*2), "Zoomed Box");
         GUILayout.Button("Zoomed Button 1");
         GUILayout.Button("Zoomed Button 2");
 
@@ -43,6 +43,11 @@ public class ZoomTestWindow : EditorWindow
         // floating point imprecision in the scaling. Therefore, it is recommended to draw the zoom
         // area first and then draw everything else so that there is no undesired overlap.
         DrawZoomArea();
+        if (EditorWindow.focusedWindow == this && Event.current.keyCode == KeyCode.R)
+        {
+            editorZoomArea.ResetZoom();
+            Event.current.Use();
+        }
     }
 }
 
@@ -54,14 +59,14 @@ public class EditorZoomArea
     private float _zoom = 1f;
     private Rect _viewArea = Rect.zero;
     private Rect _contentArea = Rect.zero;
-    private const float kZoomMin = 0.1f;
-    private const float kZoomMax = 10.0f;
+    private const float kZoomMin = 0.3f;
+    private const float kZoomMax = 3.0f;
     private const float wheelSensibility = 100.0f;
 
     public Rect Begin(Rect viewArea, Rect contentArea)
     {
         _viewArea = viewArea;
-        _contentArea = contentArea;
+        _contentArea = changeContentAreaToFitViewIfNecessary(contentArea);
         _contentArea.position = Vector2.zero;
         HandleEvents();
         GUI.EndGroup();        // End the group Unity begins automatically for an EditorWindow to clip out the window tab. This allows us to draw outside of the size of the EditorWindow.
@@ -74,10 +79,16 @@ public class EditorZoomArea
         Matrix4x4 translation = Matrix4x4.TRS(clippedArea.TopLeft(), Quaternion.identity, Vector3.one);
         Matrix4x4 scale = Matrix4x4.Scale(new Vector3(_zoom, _zoom, 1.0f));
         GUI.matrix = translation * scale * translation.inverse * GUI.matrix;
-
         GUILayout.BeginArea(new Rect(_zoomCoordsOrigin.x, _zoomCoordsOrigin.y, _viewArea.width, _viewArea.height));
 
         return clippedArea;
+    }
+
+    private Rect changeContentAreaToFitViewIfNecessary(Rect contentArea)
+    {
+        contentArea.width = Mathf.Max(contentArea.width, _viewArea.width);
+        contentArea.height = Mathf.Max(contentArea.height, _viewArea.height);
+        return contentArea;
     }
 
     private void HandleEvents()
@@ -92,18 +103,22 @@ public class EditorZoomArea
             Vector2 delta = Event.current.delta;
             Vector2 zoomCoordsMousePos = ConvertScreenCoordsToZoomCoords(screenCoordsMousePos);
             float zoomDelta = -delta.y / (wheelSensibility * 1f/_zoom);
-            if(_zoom + zoomDelta < 1){
-                zoomDelta = 0;
-                _zoom = 1;
-            }
             float oldZoom = _zoom;
             _zoom += zoomDelta;
             _zoom = Mathf.Clamp(_zoom, kZoomMin, kZoomMax);
+            if (_zoom < CalculateMinZoom())
+            {
+                _zoom = CalculateMinZoom();
+            }
             Vector2 reference = zoomCoordsMousePos;
             var movementVector = (_zoom / oldZoom) * (reference) - reference;
             _zoomCoordsOrigin -= movementVector;
-            _zoomCoordsOrigin.x = Mathf.Min(0f, _zoomCoordsOrigin.x);
-            _zoomCoordsOrigin.y = Mathf.Min(0f, _zoomCoordsOrigin.y);
+            if (_zoom == CalculateMinZoom())
+            {
+                _zoomCoordsOrigin = Vector2.zero;
+            }
+            //_zoomCoordsOrigin.x = Mathf.Min(0f, _zoomCoordsOrigin.x);
+            //_zoomCoordsOrigin.y = Mathf.Min(0f, _zoomCoordsOrigin.y);
             Event.current.Use();
         }
 
@@ -116,18 +131,10 @@ public class EditorZoomArea
             Vector2 delta = Event.current.delta;
             delta /= _zoom;
             Rect zoomMovedContentArea = new Rect(_zoomCoordsOrigin + delta, _contentArea.ScaleSizeBy(_zoom, Vector2.zero).size);
-            zoomMovedContentArea.x = Mathf.Min(0f, zoomMovedContentArea.x);
-            zoomMovedContentArea.y = Mathf.Min(0f, zoomMovedContentArea.y);
-            //var mov = zoomMovedContentArea.BotRight() - _viewArea.BotRight();
-            //if (mov.x >= 0)
-            //{
-            //    _zoomCoordsOrigin.x = zoomMovedContentArea.position.x;
-            //}
-            //if (mov.y >= 0)
-            //{
-            //    _zoomCoordsOrigin.y = zoomMovedContentArea.position.y;
-            //}
+            //zoomMovedContentArea.x = Mathf.Min(0f, zoomMovedContentArea.x);
+            //zoomMovedContentArea.y = Mathf.Min(0f, zoomMovedContentArea.y);
             _zoomCoordsOrigin = zoomMovedContentArea.position;
+            
             Event.current.Use();
         }
     }
@@ -149,6 +156,37 @@ public class EditorZoomArea
         GUI.EndGroup();
         GUI.EndGroup();
         GUI.BeginGroup(new Rect(0.0f, kEditorWindowTabHeight, Screen.width, Screen.height));
+        DrawScrollBars();
+    }
+
+    private void DrawScrollBars()
+    {
+        var widthScrollSize = _viewArea.width / _contentArea.ScaleSizeBy(_zoom, Vector2.zero).width;
+        if (widthScrollSize < 1)
+        {
+            GUI.HorizontalScrollbar(new Rect(new Vector2(_viewArea.x, _viewArea.height - 17), new Vector2(_viewArea.width - 17, 17)),
+                .1f,
+                widthScrollSize,
+                0,
+                1);
+        }
+
+        var heightScrollSize = _viewArea.height / _contentArea.ScaleSizeBy(_zoom, Vector2.zero).height;
+        if (heightScrollSize < 1)
+        {
+            GUI.VerticalScrollbar(new Rect(new Vector2(_viewArea.width - 17, 0), new Vector2(17 , _viewArea.height - 17)),
+                .1f,
+                heightScrollSize,
+                0,
+                1);
+        }
+    }
+
+    float CalculateMinZoom()
+    {
+        var minZoomWidth = _viewArea.width / _contentArea.width;
+        var minZoomHeight = _viewArea.height / _contentArea.height;
+        return Mathf.Min(minZoomHeight, minZoomWidth);
     }
 }
 
